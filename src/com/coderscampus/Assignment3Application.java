cb1129688e5d8ab7e29dec292cf6acda5adfa6c4
rimpy106 <rimpy106@gmail.com>
2022-01-20 12:44:18 -0600

    UpdateFinalCode

--- src/com/coderscampus/Assignment3Application.java
+++ src/com/coderscampus/Assignment3Application.java
@@ -1,10 +1,61 @@
 package com.coderscampus;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Assignment3Application {
+	public static User[] users = null;
+	static UserService userService = new UserService();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BufferedReader fileReader = null;
+		users = new User[4];
+		try {
+			fileReader = new BufferedReader(new FileReader("data.txt"));
+			String line;
+			int i = 0;
+			while ((line = fileReader.readLine()) != null) {
+				String[] lineData = line.split(",");
+				users[i] = new User(lineData[0], lineData[1], lineData[2]);
+				i++;
+			}
+			/*
+			 * for (int k = 0; k < users.length; k++) {
+			 * System.out.println(users[k].toString()); }
+			 */
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fileReader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
+		User checkedInUser = null;
+		int loginAttempts = 0;
+		try (Scanner scanner = new Scanner(System.in)) {
+			while (checkedInUser == null && loginAttempts < 5) {
+				System.out.println("Enter username : ");
+				String consoleUserName = scanner.nextLine();
+
+				System.out.println("Enter password : ");
+				String consolePwd = scanner.nextLine();
+				checkedInUser = userService.validateUser(consoleUserName, consolePwd);
+				loginAttempts++;
+				if (checkedInUser != null) {
+					System.out.println("Welcome: " + checkedInUser.getName());
+				}
+				if (checkedInUser == null) {
+					System.out.println("Invalid login, please try again");
+				}
+				if (loginAttempts >= 5) {
+					System.out.println("Too many failed login attempts, you are now locked out.");
+				}
+			}
+		}
 	}
-
 }
---
+++ src/com/coderscampus/UserService.java
@@ -0,0 +1,14 @@
+package com.coderscampus;
+
+public class UserService {
+	// Assignment3Application application=new Assignment3Application();
+	public User validateUser(String userName, String password) {
+		for (User user : Assignment3Application.users) {
+			if ((user.getUsername().equalsIgnoreCase(userName)) && (user.getPassword().equals(password))) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+}
